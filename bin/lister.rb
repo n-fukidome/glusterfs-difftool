0d8f94661de48358e581e00778e71a4a1b03a1a7
n-fukidome <n-fukidome@nekojarashi.com>
2015-10-22 19:22:30 +0900

    output  list for each condition

--- bin/lister.rb
+++ bin/lister.rb
@@ -149,13 +149,15 @@ class Lister
     end
 
     log "FOUND ONLY #{@list_files[0]} #{@only0.count}".blue
+    output_list "ONLY_#{ARGV[2]}", @only0
+
     log "FOUND ONLY #{@list_files[1]} #{@only1.count}".red
-    File.open("ONLY_#{ofile_name}", "a") do |f|
-      target_lists = @only0.count > @only1.count ? @only1 : @only0
-      target_lists.each{|k,v| f.puts('"' + v.first[:path] + '"')}
-    end
+    output_list "ONLY_#{ARGV[3]}", @only1
+
     log "FOUND BOTH(IDENTICAL) #{@list_files} #{@both01_iden.count}".blue
+
     log "FOUND BOTH(DIFFERENT) #{@list_files} #{@both01_diff.count}".red
+    output_list "DIFF_LIST_#{ofile_name}", @both01_diff
 
     t1 = Time.now
     log "FINISH #{__method__} #{t1-t0}".yellow
@@ -170,35 +172,11 @@ class Lister
     end
   end
 
-  # def output_only0(ofile = STDOUT)
-  #   title = "FOUND ONLY #{@list_files[0]}"
-  #   ofile.puts "<<< #{title}: #{@only0.count} >>>"
-  #   output_result("#{title}", @only0, ofile)
-  # end
-
-  # def output_only1(ofile = STDOUT)
-  #   title = "FOUND ONLY #{@list_files[1]}"
-  #   ofile.puts "<<< #{title}: #{@only1.count} >>>"
-  #   output_result("#{title}", @only1, ofile)
-  # end
-
-  # def output_both01_iden(ofile = STDOUT)
-  #   title = "FOUND BOTH(IDENTICAL) #{@list_files.join(' & ')}"
-  #   ofile.puts "<<< #{title}: #{@both01_iden.count} >>>"
-  #   # output_result("#{title}", @both01_iden, ofile)
-  # end
-
-  # def output_both01_diff(ofile = STDOUT)
-  #   title = "FOUND BOTH(DIFFERENT) #{@list_files.join(' & ')}"
-  #   ofile.puts "<<< #{title}: #{@both01_diff.count} >>>"
-  #   output_result("#{title}", @both01_diff, ofile)
-  # end
-
-  # def output_result(tag, result, ofile)
-  #   result.each do |k,v|
-  #     ofile.puts '"' + v.first[:path] + '"'
-  #   end
-  # end
+  def output_list(title, lists)
+    File.open(title, "a") do |f|
+      lists.each{|k,v| f.puts('"' + v.first[:path] + '"')}
+    end
+  end
 
   def put_count(enterprise)
     puts "#{enterprise}, #{@both01_iden.count}, #{@both01_diff.count}, #{@only0.count}, #{@only1.count},"
@@ -225,19 +203,12 @@ File.open(target_list, 'r') do |list|
     when 'compare'
       list_file0 = "lists/#{enterprise}_#{ARGV[2]}" || fail
       list_file1 = "lists/#{enterprise}_#{ARGV[3]}" || fail
-      # ofile      = "results/#{enterprise}_#{ARGV[4]}" || fail
       ofile_name = "#{ARGV[4]}" || fail
 
       lister = Lister.new(list_files: [list_file0, list_file1])
 
       lister.read_lists
       lister.compute_diff ofile_name
-      # File.open(ofile, 'w') do |f|
-      #   lister.output_only0(f)
-      #   lister.output_only1(f)
-      #   lister.output_both01_iden(f)
-      #   lister.output_both01_diff(f)
-      # end
       lister.put_count enterprise
     end
   end
--- compare_001_002.sh
+++ compare_001_002.sh
@@ -4,7 +4,7 @@ ACTION='compare'
 ACCOUNT_LIST=TARGET_LIST.lst #for example
 LIST0=LIST_bricks_sdb1_001.lst
 LIST1=LIST_bricks_sdb1_002.lst
-RES=RES_mstr001_mstr002.lst
+RES=mstr001_mstr002.lst
 CP_LOG=cp_001_002.log
 
 bundle exec ruby bin/lister.rb $ACTION $ACCOUNT_LIST $LIST0 $LIST1 $RES 2> $CP_LOG
